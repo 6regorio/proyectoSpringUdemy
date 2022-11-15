53dcb1a36a311844197d9a19a2db1583cb45a3fe
Gregorio Martinez <gregorio.martinez@mtp.es>
2022-11-15 13:01:29 +0100

    Proyecto vacío

---
+++ src/main/java/com/clientes/proyecto/ControlDeClientesApplication.java
@@ -0,0 +1,13 @@
+package com.clientes.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlDeClientesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControlDeClientesApplication.class, args);
+	}
+
+}
